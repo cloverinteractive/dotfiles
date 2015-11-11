c411bbc4aae6d3ced9b407d08460685332342351
Enrique Vidal <enrique@cloverinteractive.com>
2015-11-10 23:44:59 -0800

    Handle homebrew installation when running Darwin

--- install.sh
+++ install.sh
@@ -4,26 +4,26 @@
 START_WD=$(pwd)
 
 # All the colors we're using for notifications
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m'
+SWITCH="\033["
+RED="${SWITCH}0;31m"
+GREEN="${SWITCH}0;32m"
+YELLOW="${SWITCH}[1;33m"
+NORMAL="${SWITCH}[0m"
 
 # Print red
 function error() {
-printf "\r${RED}$1${NC}\n"
+  printf "\r${RED}$1${NORMAL}\n"
 }
 
 # Print green
 function success() {
-printf "\r${GREEN}$1${NC}\n"
+  printf "\r${GREEN}$1${NORMAL}\n"
 }
 
 # Print yellow
 function info() {
-printf "\r${YELLOW}$1${NC}\n"
+  printf "\r${YELLOW}$1${NORMAL}\n"
 }
-
 
 function install_rbenv() {
   info "Installing rbenv for you..."
@@ -87,11 +87,36 @@ function install() {
   fi
 }
 
-# Check for a path to rbenv
-if test !$(which rbenv); then
-  if test $(which git); then
-    install_rbenv
-    fetch_dotfiles
-    install
+function install_homebrew() {
+  if test ! $(which brew); then
+    info "Installing homebrew..."
+
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    if [[ $? == 0 ]]; then
+      success "Homebrew installed"
+
+      ## Load homebrew
+      echo 'export PATH="/usr/local/bin:/usr/local/sbin:$PATH"' > ~/.bash_profile.before
+      source ~/.bash_profile.before
+    fi
   fi
+}
+
+function install_git() {
+  # TODO: We need to decide wether to check the kernel and do things differently when we're not
+  # using Darwin, or install linux brew when running linux then brewing from this point onward should
+  # be exactly the same for both OS
+  # https://github.com/Homebrew/linuxbrew
+}
+
+if [[ $(uname) == "Darwin" ]]; then
+  install_homebrew
 fi
+
+if test !$(which git); then install_git; fi
+if test !$(which rbenv); then install_rbevn; fi
+
+# Fetch dotfiles from github and install
+fetch_dotfiles
+install
