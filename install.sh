f489712c39071219b043d39256e6bcf4f0753d28
Enrique Vidal <enrique@cloverinteractive.com>
2015-11-11 22:59:39 -0800

    Move from sh to bash

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Where we're at right now
-START_WD=$(pwd)
+START_WD=$(pwd -P)
 
 # All the colors we're using for notifications
 SWITCH="\033["
@@ -10,22 +10,25 @@ GREEN="${SWITCH}0;32m"
 YELLOW="${SWITCH}[1;33m"
 NORMAL="${SWITCH}[0m"
 
+set -e
+echo ''
+
 # Print red
-function error() {
+error () {
   printf "\r${RED}$1${NORMAL}\n"
 }
 
 # Print green
-function success() {
+success () {
   printf "\r${GREEN}$1${NORMAL}\n"
 }
 
 # Print yellow
-function info() {
+info () {
   printf "\r${YELLOW}$1${NORMAL}\n"
 }
 
-function install_rbenv() {
+install_rbenv () {
   info "Installing rbenv for you..."
 
   # Fetching rbenv from git
@@ -34,7 +37,7 @@ function install_rbenv() {
 
   source ~/.bash_profile.before
 
-  if test $(which rbenv); then
+  if [ $(which rbenv) ]; then
     success "rbenv installed"
     info "fetching ruby-build..."
 
@@ -46,7 +49,7 @@ function install_rbenv() {
   fi
 }
 
-function fetch_dotfiles() {
+fetch_dotfiles () {
   info "Fetching dotfiles..."
 
   git clone https://github.com/cloverinteractive/dotfiles.git ~/.dotfiles
@@ -64,7 +67,7 @@ function fetch_dotfiles() {
   fi
 }
 
-function install() {
+install () {
   info "Installing dotfiles..."
 
   cd ~/.dotfiles
@@ -87,8 +90,8 @@ function install() {
   fi
 }
 
-function install_homebrew() {
-  if test ! $(which brew); then
+install_homebrew () {
+  if ! [ $(which brew) ]; then
     info "Installing homebrew..."
 
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -103,20 +106,20 @@ function install_homebrew() {
   fi
 }
 
-function install_git() {
-  # TODO: We need to decide wether to check the kernel and do things differently when we're not
-  # using Darwin, or install linux brew when running linux then brewing from this point onward should
-  # be exactly the same for both OS
-  # https://github.com/Homebrew/linuxbrew
-}
+#install_git () {
+#  # TODO: We need to decide wether to check the kernel and do things differently when we're not
+#  # using Darwin, or install linux brew when running linux then brewing from this point onward should
+#  # be exactly the same for both OS
+#  # https://github.com/Homebrew/linuxbrew
+#}
 
 if [[ $(uname) == "Darwin" ]]; then
   install_homebrew
 fi
 
-if test !$(which git); then install_git; fi
-if test !$(which rbenv); then install_rbenv; fi
+#if ! [ $(which git) ] ; then install_git; fi
+if ! [ $(which rbenv) ]; then install_rbenv; fi
 
 # Fetch dotfiles from github and install
-fetch_dotfiles
-install
+#fetch_dotfiles
+#install
