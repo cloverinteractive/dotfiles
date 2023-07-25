4d1dfe63e0e105f468c42531d824ed635e2226e6
Enrique Vidal <enrique@Enriques-MacBook-Pro.local>
2023-07-25 14:20:26 -0700

    Update lspkind icons

--- .config/nvim/lua/plugins/lspkind/utils.lua
+++ .config/nvim/lua/plugins/lspkind/utils.lua
@@ -1,3 +1,9 @@
+local ok, lspkind = pcall(require, "lspkind")
+
+if not ok then
+    return
+end
+
 --- @alias SymbolMap { [key: string]: string }
 --
 --- @class Utils
@@ -7,60 +13,51 @@ local M = {}
 --- @type SymbolMap
 local symbols = {
     Copilot = "",
-    Text = "",
-    Method = "m",
-    Function = "ƒ",
-    Constructor = "",
-    Field = "",
-    Variable = "",
-    Class = "",
+    Text = "󰉿",
+    Method = "󰆧",
+    Function = "󰊕",
+    Constructor = "",
+    Field = "󰜢",
+    Variable = "󰀫",
+    Class = "󰠱",
     Interface = "",
     Module = "",
-    Property = "",
-    Unit = "",
-    Value = "",
+    Property = "󰜢",
+    Unit = "󰑭",
+    Value = "󰎠",
     Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
+    Keyword = "󰌋",
+    Snippet = "",
+    Color = "󰏘",
+    File = "󰈙",
+    Reference = "󰈇",
+    Folder = "󰉋",
     EnumMember = "",
-    Constant = "",
-    Struct = "",
+    Constant = "󰏿",
+    Struct = "󰙅",
     Event = "",
-    Operator = "",
-    TypeParameter = "",
+    Operator = "󰆕",
+    TypeParameter = "",
 }
 
 --- Format suggestions and add icons
 ---
 ---@return nil | unknown
 function M.add_lspkind_icons()
-    local ok, lspkind = pcall(require, "lspkind")
-
-    if ok then
-        return lspkind.cmp_format({
-            before = function(entry, vim_item)
-                vim_item.kind = string.format("%s", symbols[vim_item.kind])
-                vim_item.menu = ({
-                    buffer = "[Buffer]",
-                    luasnip = "[Snippet]",
-                    nvim_lsp = "[LSP]",
-                    path = "[path]",
-                })[entry.source.name]
-
-                return vim_item
-            end,
-            with_text = true,
-            menu = {
+    return lspkind.cmp_format({
+        before = function(entry, vim_item)
+            vim_item.kind = string.format("%s", symbols[vim_item.kind])
+            vim_item.menu = ({
+                copilot = "[Copilot]",
                 buffer = "[Buffer]",
+                luasnip = "[Snippet]",
                 nvim_lsp = "[LSP]",
                 path = "[path]",
-            },
-        })
-    end
+            })[entry.source.name]
+
+            return vim_item
+        end,
+    })
 end
 
 return M
